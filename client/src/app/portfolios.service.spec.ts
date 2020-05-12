2d04152119a351d28706f18a1305fdca91a53411
seanoshea <oshea.ie@gmail.com>
2020-05-11 22:15:55 -0400

    #39: Inject.

--- client/src/app/portfolios.service.spec.ts
+++ client/src/app/portfolios.service.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { PortfoliosService } from './portfolios.service';
 
@@ -6,7 +7,9 @@ describe('PortfoliosService', () => {
   let service: PortfoliosService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
     service = TestBed.inject(PortfoliosService);
   });
 
